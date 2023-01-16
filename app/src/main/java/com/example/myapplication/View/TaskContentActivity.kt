235f433fccfd6a15767f5faf8376fd8111289cae
Kim-Inyo <101404494+Kim-Inyo@users.noreply.github.com>
2023-01-17 01:53:54 +1000

    Activities (Final)

---
+++ app/src/main/java/com/example/myapplication/View/TaskContentActivity.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskContentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
